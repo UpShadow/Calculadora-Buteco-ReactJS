84ebfe5889418ad6b70dfe0a2a381044c689cdb6
Carlos Eduardo Correia de Souza <CarlosCorreia1002@gmail.com>
2022-05-05 12:14:42 -0300

    Índex and Control création

--- Calculadora-Buteco-ReactJS/Controllers/HomeController.cs
+++ Calculadora-Buteco-ReactJS/Controllers/HomeController.cs
@@ -1,32 +1,12 @@
-﻿using Calculadora_Buteco_ReactJS.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Calculadora_Buteco_ReactJS.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
